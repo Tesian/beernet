dfd86ae182709b60afc35a64f049446fd35351c0
Tessier <tessie_g@epitech.eu>
2012-10-30 17:48:50 +0100

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beernet::Application.config.secret_token = '052675320399e364b67648080e9313a5d72def3625bdfdf797c92dd64ed5f8051a143fa1a7b75be858f992f761ca0779fd7a8a0c9d9098824da29888773581f9'
