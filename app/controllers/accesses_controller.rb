f0d4de01f0606b7c031fd2eaeb209ef255c9b0d5
Tessier <tessie_g@epitech.eu>
2012-11-05 11:10:27 +0100

    fix bug coast destroy

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -1,3 +1,11 @@
 class AccessesController < ApplicationController
 include Coast
+
+  respond_to :destroy do
+    respond_to do |format|
+      format.html { redirect_to accesses_url }
+      format_json_and_xml(format, @resourceful_item)
+    end
+  end
+
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,10 @@
 class ClientsController < ApplicationController
 include Coast
+
+  respond_to :destroy do
+    respond_to do |format|
+      format.html { redirect_to clients_url }
+      format_json_and_xml(format, @resourceful_item)
+    end
+  end
 end
